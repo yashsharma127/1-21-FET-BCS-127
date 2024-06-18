98b09d4c0504893c98c134f973a440513b17a571
YASH SHARMA <yashshar.2323@gmail.com>
2024-06-18 13:28:50 +0530

    updating port

--- question1/src/index.js
+++ question1/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import routes from './routes.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(express.json());
 app.use('/categories', routes);
